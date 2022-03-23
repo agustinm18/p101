baa30611d8ea591fa2ea16f4ef3d6a8abf30d9fc
agustinm18 <71091200+agustinm18@users.noreply.github.com>
2022-03-23 19:44:26 -0400

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,33 @@
+import random
+from tkinter import Y
+
+response = 'y'
+
+while response == "y":
+        no=random.randint(1,6)
+        
+        if(no == 1):
+            print("[-----]")
+            print("[--0--]")
+            print("[-----]")
+        if(no == 2):
+            print("[-----]")
+            print("[-0-0-]")
+            print("[-----]")
+        if(no == 3):
+            print("[--0--]")
+            print("[--0--]")
+            print("[--0--]")
+        if(no == 4):
+            print("[-0-0-]")
+            print("[-----]")
+            print("[-0-0-]")
+        if(no == 5):
+            print("[-0-0-]")
+            print("[--0--]")
+            print("[-0-0-]")
+        if(no == 6):
+            print("[-0-0-]")
+            print("[-0-0-]")
+            print("[-0-0-]")
+        response = input("press 'y' to roll again or anything other than 'y' to exit: ")
